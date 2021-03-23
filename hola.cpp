ad5f56a926176415a51619c524645e95c93390a6
Diego González <diegom.gcalovi@gmail.com>
2021-03-23 14:12:25 -0400

    Agregamos hola platzi

--- hola.cpp
+++ hola.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-  std::cout << "HOLA MUNDO 2.0" << '\n';
+  std::cout << "¡HOLA Platzi!" << '\n';
   std::cout << 10 << '\n';
   std::cout << "10.23" << '\n';
   std::cout << true << '\n';
